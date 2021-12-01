05062a4557440e0723da1a1a128e8b68434644e4
Sean Kao <seankao31@gmail.com>
2021-11-30 19:55:56 -0600

    /user/topactive: users w/ the most recipes/reviews

--- api/app/routes/user.routes.js
+++ api/app/routes/user.routes.js
@@ -13,4 +13,6 @@ module.exports = function (app) {
   app.get("/api/v1/test/all", controller.allAccess);
 
   app.get("/api/v1/test/user", [authJwt.verifyToken], controller.userBoard);
+
+  app.get("/api/v1/user/topactive", controller.topActiveUsers);
 };
